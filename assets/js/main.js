595de4a288a64b70966520d4a6bc7a4c6dc46588
masoud <masoud.software6748@gmail.com>
2023-04-05 02:40:30 +0430

    The answer to the second question

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,25 @@ let user = {
   },
 };
 user.sayHello();
+
+// --------- 2 ---------
+const stopWatch = {
+  steps: 0,
+  increase() {
+    this.steps++;
+    return this;
+  },
+  decrease() {
+    this.steps--;
+    return this;
+  },
+  reset() {
+    this.steps = 0;
+    return this;
+  },
+  read() {
+    console.log(`تعداد قدم : ${this.steps}`);
+    return this;
+  },
+};
+stopWatch.increase().increase().increase().decrease().read();
